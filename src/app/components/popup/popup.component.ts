60e6aa36e912d892e0cfdd123a8ad78f59fca647
VALSEV3 <valeri.sevyan@tumo.org>
2024-10-13 00:17:32 +0400

    Update popup.component.ts

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,23 +1,44 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-popup',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
 export class PopupComponent {
   balance = '';
 
-
-  constructor(private router:Router) {}
+  constructor(private router: Router) {}
 
   topUp() {
-    alert(`you top up your balance ${this.balance}`)
+    // Проверим, что баланс введен корректно
+    if (!this.balance || isNaN(Number(this.balance))) {
+      alert('Please enter a valid amount.');
+      return;
+    }
+
+    alert(`You top up your balance: $${this.balance}`);
     localStorage.setItem('balance', this.balance);
-    console.log(this.balance);
-    this.router.navigate(['/home-page'])
+
+    // Закрываем модальное окно
+    this.closeModal();
+
+    // Выполняем перенаправление после закрытия модального окна
+    this.router.navigate(['/home-page']);
+  }
+
+  closeModal() {
+    // Получаем элемент модального окна по ID
+    const modalElement = document.getElementById('exampleModal');
+    if (modalElement) {
+      // Используем Bootstrap modal для закрытия
+      const modal = bootstrap.Modal.getInstance(modalElement);
+      if (modal) {
+        modal.hide();
+      }
+    }
   }
 }
